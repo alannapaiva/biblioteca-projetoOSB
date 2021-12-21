aec9bb76519022bdae94a13c562418387bdfa4bf
Alanna <alannapaiva1@hotmail.com>
2021-12-21 17:34:00 -0300

    create the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { AiOutlineSearch } from "react-icons/ai";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    	<header className="App-header">
+			<h1>Biblioteca</h1>
+			<input type="text"></input>
+      	</header>
+		  <button><AiOutlineSearch size={45}/> </button>
     </div>
   );
 }
