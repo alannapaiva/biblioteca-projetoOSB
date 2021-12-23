36e44f85f71f39ea5cfa1583ac1c3b1c0382609e
Alanna <alannapaiva1@hotmail.com>
2021-12-23 18:57:47 -0300

    finisih and add to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import { AiOutlineSearch } from "react-icons/ai";
-import './App.css';
+import React, {useState} from 'react';
+import { Livros } from "./components/Livros";
 
 function App() {
   return (
     <div className="App">
-    	<header className="App-header">
-			<h1>Biblioteca</h1>
-			<input type="text"></input>
-      	</header>
-		  <button><AiOutlineSearch size={45}/> </button>
+		  <Livros />
     </div>
   );
 }
